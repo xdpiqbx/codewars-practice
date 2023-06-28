95afd17bd0ac8ff119bb5c3b61cdfcd8b6592e9f
xdpiqbx <xdpiqbx@gmail.com>
2023-06-28 12:58:04 +0300

    in process

--- katanki.py
+++ katanki.py
@@ -1310,10 +1310,34 @@
 #     return [[1 for _ in range(i+1)] for i in range(n)]
 #     # return [[1] * i for i in range(1, n+1)]
 
+# =============================================================== Product of Largest Pair 0174
+# def max_product(a):
+#     a.sort()
+#     return a[0] * a[1]
+# Completed in 0.13ms
 
-
-
-
-
-
+# def max_product(a):
+#     max1 = 0
+#     max2 = 0
+#
+#     for num in a:
+#         if num > max1:
+#             max2 = max1
+#             max1 = num
+#         elif num > max2:
+#             max2 = num
+#
+#     return max1 * max2
+#
+#     # m1 = max(a)
+#     # a.remove(m1)
+#     # m2 = max(a)
+#     # return m1 * m2
+#
+# # import heapq
+# # def max_product(a):
+# #     import heapq
+# #     x, y = heapq.nlargest(2, a)
+# #     return x * y
+# print(max_product([56, 335, 195, 443, 6, 494, 252]))
 
